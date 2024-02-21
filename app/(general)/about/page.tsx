9a4ef2df2a714216110fc9f3035797a507b2bd8d
Victor Leon <victorlyonfuentes1998@gmail.com>
2024-02-21 15:36:09 -0400

    first commit

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,16 @@
+
+export const metadata = {
+ title: 'About page',
+ description: 'SEO Title',
+ keywords:['About Page','Victor','Informacion','...']
+};
+
+export default function AboutPage(){
+    return(
+        <>
+
+        <span className="text-7xl">About page</span>
+ 
+        </>
+    )
+}
---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,15 @@
+
+export const metadata = {
+    title: 'Contact page',
+    description: 'SEO Title',
+    keywords:['Contact Page','Victor','Informacion','...']
+   };
+   
+
+export default function ContactPage(){
+    return(
+        <>
+            <span className="text-7xl">Contact page</span>
+        </>
+    )
+}
